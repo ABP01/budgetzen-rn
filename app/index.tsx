b7264c1f32667cc9133db4cd309f4e894485d5ef
Armel Bogue <komlanbogue@gmail.com>
2025-09-20 22:23:22 +0000

    refactor: update layout and index components for improved structure and styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, Image, View } from 'react-native'
+import React, { useEffect } from 'react'
+import { colors } from '@/constants/theme'
+import { useRouter } from 'expo-router'
 
-export default function index() {
+const Index = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+   setTimeout(() => {
+     router.push('/welcome');
+   }, 2000);
+
+}, []);
+
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        resizeMode="contain"
+        source={require('../assets/images/splashImage.png')}
+      />
+      <Text style={styles.title}>BudgetZen</Text>
     </View>
-  )
-}
+  );
+};
 
-const styles = StyleSheet.create({})
+export default Index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.neutral900,
+  },
+  logo: {
+    height: '20%',
+    aspectRatio: 1,
+  },
+  title: {
+    color: colors.neutral50,
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 16,
+  },
+});
