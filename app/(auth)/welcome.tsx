6b5420a7098abaa873a0b5a036005bd17c0d7590
Armel Bogue <komlanbogue@gmail.com>
2025-09-21 22:01:46 +0000

    feat: enhance welcome screen with new layout, image, and footer content

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,55 +1,88 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-import ScreenWrapper from '@/components/ScreenWrapper'
-import Typo from '@/components/Typo'
-import { spacingX, spacingY } from '@/constants/theme'
-import { verticalScale } from '@/utils/styling'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
 
 const welcome = () => {
   return (
     <ScreenWrapper>
       <View style={styles.container}>
-        <TouchableOpacity>
-            <Typo></Typo>
-        </TouchableOpacity>
+        <View>
+          <TouchableOpacity style={styles.loginButton}>
+            <Typo fontWeight={"500"}>Sign In</Typo>
+          </TouchableOpacity>
+
+          <Image
+            source={require("../../assets/images/welcome.png")}
+            style={styles.welcomeImage}
+            resizeMode="contain"
+          />
         </View>
+
+        {/* footer */}
+<View style={styles.footer}>
+  <View style={{ alignItems: "center" }}>
+    <Typo size={30} fontWeight={"800"}>
+      Always take control
+    </Typo>
+    <Typo size={30} fontWeight={"800"}>
+      of your finances
+    </Typo>
+  </View>
+
+  <View style={{ alignItems: "center", gap: 2 }}>
+    <Typo size={17} color={colors.textLight}>
+      Finances must be arranged to set a better
+    </Typo>
+    <Typo size={17} color={colors.textLight}>
+      lifestyle in future
+    </Typo>
+  </View>
+
+  <View style={styles.buttonContainer}>
+    {/* button */}
+  </View>
+</View>
+
+      </View>
     </ScreenWrapper>
-  )
-}
+  );
+};
 
 export default welcome;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "space-between",
-        paddingTop: spacingY._7,
-    },
-    welcomeImage: {
-        width: '100%',
-        height: verticalScale(300),
-        alignSelf: 'center',
-        marginTop: verticalScale(100),
-    },
-    loginButton: {
-        alignSelf: 'flex-end',
-        marginRight: spacingY._20,
-    },
-    footer: {
-        alignItems: 'center',
-        backgroundColor: 'colors.neutral900',
-        paddingTop: verticalScale(30),
-        paddingBottom: verticalScale(45),
-        gap: spacingY._20,
-        shadowColor: 'white',
-        shadowOffset: { width: 0, height: -10 },
-        elevation: 10,
-        shadowOpacity: 0.15,
-        shadowRadius: 25,
-    },
-    buttonContainer: {
-        width: '100%',
-        paddingHorizontal: spacingX._25,
-    },
-
-})
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingTop: spacingY._7,
+  },
+  welcomeImage: {
+    width: "100%",
+    height: verticalScale(300),
+    alignSelf: "center",
+    marginTop: verticalScale(100),
+  },
+  loginButton: {
+    alignSelf: "flex-end",
+    marginRight: spacingX._20,
+  },
+  footer: {
+    alignItems: "center",
+    backgroundColor: "colors.neutral900",
+    paddingTop: verticalScale(30),
+    paddingBottom: verticalScale(45),
+    gap: spacingY._20,
+    shadowColor: "white",
+    shadowOffset: { width: 0, height: -10 },
+    elevation: 10,
+    shadowOpacity: 0.15,
+    shadowRadius: 25,
+  },
+  buttonContainer: {
+    width: "100%",
+    paddingHorizontal: spacingX._25,
+  },
+});
