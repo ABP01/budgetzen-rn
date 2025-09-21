545405338629f2d2d8b8465279e2f83b0c7d130a
Armel Bogue <komlanbogue@gmail.com>
2025-09-21 20:14:23 +0000

    feat: add Codemagic workflow and enhance welcome screen with new components

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,14 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import ScreenWrapper from '@/components/ScreenWrapper'
+import Typo from '@/components/Typo'
+import { spacingX, spacingY } from '@/constants/theme'
+import { verticalScale } from '@/utils/styling'
 
 const welcome = () => {
   return (
-    <View>
-      <Text>welcome</Text>
-    </View>
+    <ScreenWrapper>
+      <View style={styles.container}>
+        <TouchableOpacity>
+            <Typo></Typo>
+        </TouchableOpacity>
+        </View>
+    </ScreenWrapper>
   )
 }
 
-export default welcome
+export default welcome;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "space-between",
+        paddingTop: spacingY._7,
+    },
+    welcomeImage: {
+        width: '100%',
+        height: verticalScale(300),
+        alignSelf: 'center',
+        marginTop: verticalScale(100),
+    },
+    loginButton: {
+        alignSelf: 'flex-end',
+        marginRight: spacingY._20,
+    },
+    footer: {
+        alignItems: 'center',
+        backgroundColor: 'colors.neutral900',
+        paddingTop: verticalScale(30),
+        paddingBottom: verticalScale(45),
+        gap: spacingY._20,
+        shadowColor: 'white',
+        shadowOffset: { width: 0, height: -10 },
+        elevation: 10,
+        shadowOpacity: 0.15,
+        shadowRadius: 25,
+    },
+    buttonContainer: {
+        width: '100%',
+        paddingHorizontal: spacingX._25,
+    },
+
+})
