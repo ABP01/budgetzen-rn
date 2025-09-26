4291d457c45e5e59f7b531a5d9c2f68a9aa3b39b
Armel Bogue <komlanbogue@gmail.com>
2025-09-26 12:41:17 +0000

    feat: update welcome screen with animated components and new button for registration

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,20 +1,25 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
+import Button from "@/components/Button";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
 import { colors, spacingX, spacingY } from "@/constants/theme";
 import { verticalScale } from "@/utils/styling";
+import { useRouter } from "expo-router";
+import React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import Animated, { FadeIn, FadeInDown } from "react-native-reanimated";
 
-const welcome = () => {
+const Welcome = () => {
+  const router = useRouter();
   return (
     <ScreenWrapper>
       <View style={styles.container}>
         <View>
-          <TouchableOpacity style={styles.loginButton}>
+          <TouchableOpacity onPress={() => router.push("/(auth)/login" as any)} style={styles.loginButton}>
             <Typo fontWeight={"500"}>Sign In</Typo>
           </TouchableOpacity>
 
-          <Image
+          <Animated.Image
+            entering={FadeIn.duration(1000)}
             source={require("../../assets/images/welcome.png")}
             style={styles.welcomeImage}
             resizeMode="contain"
@@ -22,36 +27,53 @@ const welcome = () => {
         </View>
 
         {/* footer */}
-<View style={styles.footer}>
-  <View style={{ alignItems: "center" }}>
-    <Typo size={30} fontWeight={"800"}>
-      Always take control
-    </Typo>
-    <Typo size={30} fontWeight={"800"}>
-      of your finances
-    </Typo>
-  </View>
+        <View style={styles.footer}>
+          <Animated.View
+            entering={FadeInDown.duration(1000).springify().damping(12)}
+            style={{ alignItems: "center" }}
+          >
+              <Typo size={30} fontWeight={"800"}>
+                Always take control
+              </Typo>
+              <Typo size={30} fontWeight={"800"}>
+                of your finances
+              </Typo>
+            </Animated.View>
 
-  <View style={{ alignItems: "center", gap: 2 }}>
-    <Typo size={17} color={colors.textLight}>
-      Finances must be arranged to set a better
-    </Typo>
-    <Typo size={17} color={colors.textLight}>
-      lifestyle in future
-    </Typo>
-  </View>
+            <Animated.View
+              entering={FadeInDown.duration(1000)
+                .delay(100)
+                .springify()
+                .damping(12)}
+              style={{ alignItems: "center", gap: 2 }}
+            >
+              <Typo size={17} color={colors.textLight}>
+                Finances must be arranged to set a better
+              </Typo>
+              <Typo size={17} color={colors.textLight}>
+                lifestyle in future
+              </Typo>
+            </Animated.View>
 
-  <View style={styles.buttonContainer}>
-    {/* button */}
-  </View>
-</View>
+            <Animated.View
+              entering={FadeInDown.duration(1000).delay(200).springify().damping(12)}
+              style={styles.buttonContainer}
+            >
+              <Button onPress={() => router.push("/(auth)/register" as any)}>
+                <Typo size={18} fontWeight={"600"} color={colors.neutral900}>
+                  Get Started
+                </Typo>
+              </Button>
+            </Animated.View>
+        </View>
+
 
       </View>
-    </ScreenWrapper>
+    </ScreenWrapper >
   );
 };
 
-export default welcome;
+export default Welcome;
 
 const styles = StyleSheet.create({
   container: {
@@ -71,7 +93,7 @@ const styles = StyleSheet.create({
   },
   footer: {
     alignItems: "center",
-    backgroundColor: "colors.neutral900",
+    backgroundColor: colors.neutral900,
     paddingTop: verticalScale(30),
     paddingBottom: verticalScale(45),
     gap: spacingY._20,
